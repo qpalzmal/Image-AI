148301b5787dbdd3a7cab21be65c0f73acb88459
Morgan Shan <mshan0290@gmail.com>
2019-01-10 10:39:02 -0700

    working prediction

--- ImageRecognition1.py
+++ ImageRecognition1.py
@@ -4,12 +4,12 @@ import os
 execution_path = os.getcwd()  # gets the path of where the python file and model file are at
 
 prediction = CustomImagePrediction()
-prediction.setModelTypeAsDenseNet()  # sets the model type of prediction as ResNet - one we used
+prediction.setModelTypeAsResNet()  # sets the model type of prediction as ResNet - one we used
 prediction.setModelPath("idenprof_061-0.7933.h5")  # sets model path of prediction to the ai model file
 prediction.setJsonPath("idenprof_model_class.json")  # sets the path of json file
 prediction.loadModel(num_objects=10)
 
-prediction, probabilities = prediction.predictImage("image.jpg", result_count=3)
+predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction, ":", eachProbability)
