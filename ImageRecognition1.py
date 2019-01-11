1067ea6b4ea9dda34a812faea03b4c74a23bb189
Morgan Shan <mshan0290@gmail.com>
2019-01-11 11:44:01 -0700

    small fix to value error

--- ImageRecognition1.py
+++ ImageRecognition1.py
@@ -1,9 +1,10 @@
 from imageai.Prediction.Custom import CustomImagePrediction
-import os
+# import os
 
-execution_path = os.getcwd()  # gets the path of where the python file and model file are at
+# execution_path = os.getcwd()  # gets the path of where the python file and model file are at
 
 prediction = CustomImagePrediction()
+
 prediction.setModelTypeAsResNet()  # sets the model type of prediction as ResNet - one we used
 prediction.setModelPath("idenprof_061-0.7933.h5")  # sets model path of prediction to the ai model file
 prediction.setJsonPath("idenprof_model_class.json")  # sets the path of json file
@@ -11,5 +12,5 @@ prediction.loadModel(num_objects=10)
 
 predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
 
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction, ":", eachProbability)
+for PREDICTION, PROBABILITY in zip(predictions, probabilities):
+    print(PREDICTION, ":", PROBABILITY, "%")
