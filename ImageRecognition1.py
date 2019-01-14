ab17c7b7a745167179cc8a94008e37e045b67819
Morgan Shan <mshan0290@gmail.com>
2019-01-14 10:38:13 -0700

    object detection and experimental image/object detection

--- ImageRecognition1.py
+++ ImageRecognition1.py
@@ -1,4 +1,5 @@
 from imageai.Prediction.Custom import CustomImagePrediction
+from imageai.Detection import ObjectDetection
 # import os
 
 # execution_path = os.getcwd()  # gets the path of where the python file and model file are at
@@ -10,7 +11,13 @@ prediction.setModelPath("idenprof_061-0.7933.h5")  # sets model path of predicti
 prediction.setJsonPath("idenprof_model_class.json")  # sets the path of json file
 prediction.loadModel(num_objects=10)
 
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
+detector.loadModel()
+detections = detector.detectObjectsFromImage("image.jpg", "newimage.jpg")
+
 predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
 
-for PREDICTION, PROBABILITY in zip(predictions, probabilities):
-    print(PREDICTION, ":", PROBABILITY, "%")
+for prediction, probability in zip(predictions, probabilities):
+    print(prediction, ":", probability)
