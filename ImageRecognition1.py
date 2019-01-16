9a29c4830d6034e281fb75a5f7f983626ff15455
Morgan Shan <mshan0290@gmail.com>
2019-01-16 10:57:23 -0700

    more test programs

--- ImageRecognition1.py
+++ ImageRecognition1.py
@@ -5,6 +5,8 @@ import pprint
 
 # execution_path = os.getcwd()  # gets the path of where the python file and model file are at
 
+
+# DETECTION TO OUTPUT THE PREDICTION IN CONSOLE
 prediction = CustomImagePrediction()
 
 prediction.setModelTypeAsResNet()  # sets the model type of prediction as ResNet - one we used
@@ -12,18 +14,18 @@ prediction.setModelPath("idenprof_061-0.7933.h5")  # sets model path of predicti
 prediction.setJsonPath("idenprof_model_class.json")  # sets the path of json file
 prediction.loadModel(num_objects=10)
 
-# print("AAAAAAAAA")
-
+# DETECTION TO CREATE NEW IMAGES ON FOUND ITEMS
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
 detector.loadModel()
 detections, path = detector.detectObjectsFromImage("image.jpg",  # takes in the input image
                                                    output_image_path="newimage.jpg",  # creates a new image
-                                                   minimum_percentage_probability=25,
-                                                   # minimum percentage needed to show
-                                                   extract_detected_objects=True)  # creates new images from the each object
+                                                   # creates new images from the each object
+                                                   extract_detected_objects=True)
 
+
+# DETECTION TO DRAW BOXES AROUND IN A NEW IMAGE
 lidl_detector = ObjectDetection()
 lidl_detector.setModelTypeAsRetinaNet()
 lidl_detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
@@ -31,19 +33,25 @@ lidl_detector.loadModel()
 lidl_detections = lidl_detector.detectObjectsFromImage("image.jpg",  # takes in the input image
                                                        output_image_path="LIDLnewimage.jpg")  # creates a new image
 
-# print("BBBBBBBBB")
-
 predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
 
+# DETECTION TO OUTPUT THE PREDICTION IN CONSOLE
 for prediction, probability in zip(predictions, probabilities):
     print(prediction, ":", probability)
 
+# DETECTION TO CREATE NEW IMAGES ON FOUND ITEMS
+for eaobject, eapath in zip(detections, path):
+    print("DECTECTION: ", eaobject, "PATH :", eapath)
+    print("________________________________________")
+
+
+# DETECTION TO DRAW BOXES AROUND IN A NEW IMAGE
 for object in lidl_detections:
     #     pprint.pprint(object)
     #     # print("REE")
     print(object["name"], ":", object["percentage_probability"])
     print("___________________________________________________")
 
-for eaobject, eapath in zip(detections, path):
-    print("DECTECTION: ", eaobject, "PATH :", eapath)
-    print("________________________________________")
+print(detections)
+
+
---
+++ VideoObjectDetection1.py
@@ -0,0 +1,2 @@
+from imageai.Detection import VideoObjectDetection
+
