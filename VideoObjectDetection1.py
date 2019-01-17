a21fb31e12f372a59dcc57a8788afc1c409cd38f
Morgan Shan <mshan0290@gmail.com>
2019-01-17 10:39:04 -0700

    video object detection

--- VideoObjectDetection1.py
+++ VideoObjectDetection1.py
@@ -1,9 +1,17 @@
 from imageai.Detection import VideoObjectDetection
+import cv2
+
+camera = cv2.VideoCapture(0)
+camera.
 
 detector = VideoObjectDetection()
-detector.setModelTypeAsYOLOv3()
+detector.setModelTypeAsTinyYOLOv3()
 detector.setModelPath("yolo-tiny.h5")
-detector.loadModel()
-detections = detector.detectObjectsFromVideo(input_file_path=,
-                                             output_file_path=,
-                                             frames_per_second=60)
+detector.loadModel(detection_speed="flash")
+detections = detector.detectObjectsFromVideo(input_file_path="Road traffic video for object recognition.mp4",
+                                             output_file_path="REEEEEE.mp4",  # new video that has the boxes for objects
+                                             # camera_input=camera,
+                                             frames_per_second=60,
+                                             minimum_percentage_probability=5)
+
+print("REEEEEEEEEEEEEEEEEEEEE")
