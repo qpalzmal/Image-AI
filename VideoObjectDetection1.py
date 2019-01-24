1689ac227c908d404b3d973233c6a4663f47a86a
Morgan Shan <mshan0290@gmail.com>
2019-01-24 10:39:11 -0700

    small fix to work

--- VideoObjectDetection1.py
+++ VideoObjectDetection1.py
@@ -1,7 +1,6 @@
 from imageai.Detection import VideoObjectDetection
 import cv2
 
-
 # def EveryFrame(frame_number, output_array, output_count):
 #     print("FOR FRAME: ", frame_number)
 #     print(output_array)
@@ -14,22 +13,21 @@ camera = cv2.VideoCapture(0)
 detector = VideoObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
-# detector.loadModel(detection_speed="flash")
+detector.loadModel(detection_speed="faster")
 
 # print("BEFORE CAMERA RELEASE")
 # camera.release()
 # print("AFTER CAMERA RELEASE")
 
 detections = detector.detectObjectsFromVideo(
-                                             # input_file_path="Road traffic video for object recognition.mp4",
+                                             input_file_path="Road traffic video for object recognition.mp4",
                                              output_file_path="REEEEEE",  # new video that has the boxes for objects
-                                             camera_input=camera,
                                              frames_per_second=10,
                                              minimum_percentage_probability=50,
                                              log_progress=True
                                              # return_detected_frame=True,
                                              # per_frame_function=EveryFrame
-                                             )
+)
 
 # for object in detections:
 #     print(object["name"], ":", object["percentage_probability"])
