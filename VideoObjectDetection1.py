d94d4e07ba9393ee9eaa72e5ad52cf4d3f356c7b
Morgan Shan <mshan0290@gmail.com>
2019-02-01 10:38:09 -0700

    outputs the time in video to detected objects

--- VideoObjectDetection1.py
+++ VideoObjectDetection1.py
@@ -2,9 +2,23 @@ from imageai.Detection import VideoObjectDetection
 
 
 def EveryFrame(frame_number, output_array, output_count):
-    print("OUTPUT FOR EACH OBJECT: ", output_array)
-    print("OUTPUT COUNT FOR UNIQUE OBJECTS: ", output_count)
-    # print("END OF FRAME: ", frame_number)
+    # print("OUTPUT FOR EACH OBJECT:", output_array)
+    print("OUTPUT COUNT FOR UNIQUE OBJECTS:", output_count)
+
+    fps = 29  # fps that the script reports for 1 second of the video
+    # "Sounds of New York City".mp4 = 29 fps
+    # "Road traffic video for object recognition".mp4 = 25 fps
+
+    # used to convert the seconds to usual hrs:mins:secs format
+    seconds = frame_number / fps
+    minutes = 0
+    hours = 0
+    while minutes - 60 >= 0 or seconds - 60 >= 0:
+        if seconds - 60 >= 0:
+            minutes += 1
+        else:
+            hours += 1
+    print("TIME:", str(hours) + ":" + str(minutes) + ":" + str(seconds.__round__(2)))
     print("")
 
 
@@ -13,20 +27,17 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
 
 # 1 = bad ---- 5 = good
-detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
-# detector.loadmodel(detection_speed="faster")  # 3 speed 3 accuracy
-# detector.loadmodel(detection_speed="flash")  # 5 speed 1 accuracy
+# detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
+detector.loadModel(detection_speed="faster")  # 3 speed 3 accuracy
+# detector.loadModel(detection_speed="flash")  # 5 speed 1 accuracy
 
 
-detections = detector.detectObjectsFromVideo(input_file_path="Sounds of New York City.mp4",
-                                             output_file_path="Videos\NEW YORK",
-                                             frames_per_second=30,
+detections = detector.detectObjectsFromVideo(input_file_path="Road traffic video for object recognition.mp4",
+                                             output_file_path="Videos\\ROAD",
+                                             frames_per_second=29,
                                              minimum_percentage_probability=50,
                                              log_progress=True,
                                              per_frame_function=EveryFrame)
 
-# for object in detections:
-#     print(object["name"], ":", object["percentage_probability"])
-
-# camera.release()
+# only executes once the whole video has been analyzed
 print("REEEEEEEEEEEEEEEEEEEEE")
