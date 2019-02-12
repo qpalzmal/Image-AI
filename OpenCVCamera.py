02c00b3b80671af003dd0340a1a35a67222b2a8c
Morgan Shan <mshan0290@gmail.com>
2019-02-12 10:39:15 -0700

    removed async to test if works

--- OpenCVCamera.py
+++ OpenCVCamera.py
@@ -2,36 +2,57 @@
 # Uses webcam and displays what it captures
 import cv2
 import time
-import asyncio
+# import asyncio
 
 camera = cv2.VideoCapture(0)
 
 
-async def show_webcam(cam, mirror=False):
-    while True:
-        ret, frame = cam.read()  # captures frame by frame
-        if mirror:
-            frame = cv2.flip(frame, 1)
-        # display the resulting frame
-        cv2.imshow("Webcam", frame)
+# def show_webcam(cam, mirror=False):
+#     while True:
+#         ret, frame = cam.read()  # captures frame by frame
+#         if mirror:
+#             frame = cv2.flip(frame, 1)
+#         # display the resulting frame
+#         cv2.imshow("Webcam", frame)
+#
+#         # print("TRY TIME SLEEP")
+#         # time.sleep(1)
+#         # print("DONE TIME SLEEP")
+#         # print("")
+#         # print("TRY ASYNCIO SLEEP")
+#         # await asyncio.sleep(1)
+#         # print("DONE ASYNCIO SLEEP")
+#         # print("")
+#
+#         if cv2.waitKey(27) == 27:
+#             break  # esc to quit
+#     cam.release()
+#     cv2.destroyAllWindows()
 
-        # print("TRY TIME SLEEP")
-        # time.sleep(1)
-        # print("DONE TIME SLEEP")
-        # print("")
-        # print("TRY ASYNCIO SLEEP")
-        # await asyncio.sleep(1)
-        # print("DONE ASYNCIO SLEEP")
-        # print("")
 
-        if cv2.waitKey(27) == 27:
-            break  # esc to quit
-    cam.release()
+def show_webcam(cam, mirror=False):
+    ret, frame = cam.read()  # captures frame by frame
+    if mirror:
+        frame = cv2.flip(frame, 1)
+    # display the resulting frame
+    cv2.imshow("Webcam", frame)
+
+    # time.sleep(3)
+
+    # functions similar to time.sleep but doesn't suspend webcam
+    old_time = time.time()
+    current_time = 0
+    while current_time - old_time <= 3:
+        current_time = time.time()
+        print(current_time, "CURRENT")
+        print(old_time, "OLD")
+
+    # cam.release()
     cv2.destroyAllWindows()
 
 
-async def main():
-    await show_webcam(cam=camera, mirror=True)
+def main():
+    show_webcam(cam=camera, mirror=True)
 
 
 if __name__ == "__main__":
--- WebcamObjectDetection.py
+++ WebcamObjectDetection.py
@@ -1,45 +1,46 @@
 from imageai.Detection import VideoObjectDetection
 import OpenCVCamera as cvcamera
 import asyncio
+import time
 
 
-def call_everyframe(frame_number, output_array, output_count):
-    loop = asyncio.new_event_loop()
-    frame_task = loop.create_task(EveryFrame(frame_number, output_array, output_count))
-    loop.run_until_complete(frame_task)
+# def call_everyframe(frame_number, output_array, output_count):
+#     loop = asyncio.new_event_loop()
+#     frame_task = loop.create_task(EveryFrame(frame_number, output_array, output_count))
+#     loop.run_until_complete(frame_task)
 
 
-async def EveryFrame(frame_number, output_array, output_count):
+def EveryFrame(frame_number, output_array, output_count):
     # print(output_array)
     print(output_count)
     # print("END OF FRAME: ", frame_number)
     print("")
-    await asyncio.sleep(1)
+    cvcamera.main()
 
-
-camera = cvcamera.camera
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
 
 # 1 = bad ---- 5 = good
-detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
-# detector.loadmodel(detection_speed="faster")  # 3 speed 3 accuracy
-# detector.loadmodel(detection_speed="flash")  # 5 speed 1 accuracy
+# detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
+detector.loadModel(detection_speed="faster")  # 3 speed 3 accuracy
+
+
+# detector.loadModel(detection_speed="flash")  # 5 speed 1 accuracy
 
 
 # calling a async function returns a coroutine object and doesn't run them
-async def DetectObjects():
-    await detector.detectObjectsFromVideo(output_file_path="Videos\\WEBCAM",
-                                          frames_per_second=30,  # LAPTOP WEBCAM RUNS AT 30 FPS
-                                          minimum_percentage_probability=50,
-                                          log_progress=True,
-                                          camera_input=camera,
-                                          per_frame_function=EverFrame)
+def DetectObjects():
+    detector.detectObjectsFromVideo(output_file_path="Videos\\WEBCAM",
+                                    frames_per_second=30,  # LAPTOP WEBCAM RUNS AT 30 FPS
+                                    minimum_percentage_probability=50,
+                                    log_progress=True,
+                                    camera_input=cvcamera.camera,
+                                    per_frame_function=EveryFrame)
 
 
-async def main():
+def main():
     # loop = asyncio.new_event_loop()
 
     # detect_task = loop.create_task(DetectObjects())
@@ -48,10 +49,9 @@ async def main():
     # loop.run_until_complete(detect_task)
     # loop.run_until_complete(show_webcam_task)
 
-    await DetectObjects()
-    await cvcamera.show_webcam(cam=camera)
+    # await DetectObjects()
+    DetectObjects()
 
 
 if __name__ == "__main__":
-    main_loop = asyncio.new_event_loop()
-    main_loop.run_until_complete(main())
+    main()
