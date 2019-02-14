bf77f08f6a4d3a4a9f07e213478c8bc939d0a6bc
Morgan Shan <mshan0290@gmail.com>
2019-02-14 10:30:39 -0700

    fps change

--- WebcamObjectDetection.py
+++ WebcamObjectDetection.py
@@ -14,14 +14,14 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath("resnet50_coco_best_v2.0.1.h5")
 
 # 1 = bad ---- 5 = good
-detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
-# detector.loadModel(detection_speed="faster")  # 3 speed 3 accuracy
+# detector.loadModel(detection_speed="normal")  # 1 speed 5 accuracy
+detector.loadModel(detection_speed="faster")  # 3 speed 3 accuracy
 # detector.loadModel(detection_speed="flash")  # 5 speed 1 accuracy
 
 
 # per_frame_function passes in all of the present parameters
 def EveryFrame(frame_number, output_array, output_count):
-    cv.main()  # displays the image that is captured by the webcam
+    # cv.main()  # displays the image that is captured by the webcam
     # print(output_array)  # prints out each individual object, name, and box location
     print(output_count)  # prints out the unique count for different objects
     print("")
@@ -30,7 +30,7 @@ def EveryFrame(frame_number, output_array, output_count):
 # calling a async function returns a coroutine object and doesn't run them
 def DetectObjects():
     detector.detectObjectsFromVideo(output_file_path="Videos\\WEBCAM",
-                                    frames_per_second=30,  # LAPTOP WEBCAM RUNS AT 30 FPS
+                                    frames_per_second=15,  # LAPTOP WEBCAM RUNS AT 30 FPS
                                     minimum_percentage_probability=50,
                                     log_progress=True,
                                     camera_input=cv.camera,
